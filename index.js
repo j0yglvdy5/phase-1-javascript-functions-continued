a51e8651e93b70f71b59b6cc255345b29a10feb0
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-04-09 16:14:48 +0300

    phase 1

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+  }
+
+function wrapAdjective(flair = '*') {
+    return function(adjective = 'a hard worker') {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  const encouragingPromptFunction = wrapAdjective('!!!');
+console.log(encouragingPromptFunction('a dedicated programmer'));
+
+
+const emphasizeFunction = wrapAdjective('||');
+console.log(emphasizeFunction('a hard worker'));
+
